3da99a6d7833ec21e16affb85701c5bf10a413c1
duxinyues <yongyuan253015@gmail.com>
2021-07-20 00:10:54 +0800

    设置本年第一天和最后一天

--- src/components/page1.jsx
+++ src/components/page1.jsx
@@ -3,7 +3,7 @@
  * @Author: duxinyue
  * @Date: 2021-05-15 11:54:32
  * @LastEditors: yongyuan at <yongyuan253015@gmail.com>
- * @LastEditTime: 2021-07-20 00:04:15
+ * @LastEditTime: 2021-07-20 00:10:07
  * @FilePath: \router\src\components\page1.jsx
  * @Description: 
  */
@@ -23,14 +23,15 @@ function Toolbar(props) {
         setAll(true)
     }
     const onMonth = () => {
-        var nowDate = new Date();
-        console.log(new Date(nowDate.getFullYear(), 0, 0).getDate())
+
         setAll(false)
         setDate([moment(getFirstAndEndDay(2), dateFormat), moment(getFirstAndEndDay(1), dateFormat)])
     }
     const onYear = () => {
+        var nowDate = new Date();
+        console.log(nowDate.getFullYear())
         setAll(false)
-        setDate([moment(getYearFirstLastDay()[0], dateFormat), moment(getYearFirstLastDay()[1], dateFormat)])
+        setDate([moment(`${nowDate.getFullYear()}-01-01`, dateFormat), moment(`${nowDate.getFullYear()}-12-31`, dateFormat)])
     }
     return <React.Fragment>
         <Space direction="horizontal" size={12}> <span onClick={allDate}>所有</span> <span onClick={onMonth}>本月</span> <span onClick={onYear}>今年</span></Space>
