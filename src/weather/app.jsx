31ec8a746c67f3c269c00bc634dae9dad329dee2
读心悦 <1638877065@qq.com>
2022-03-24 14:03:42 +0800

    webpack 构建react实例

--- src/weather/app.jsx
+++
@@ -1,8 +0,0 @@
-import WeatherView from './weatherView';
-import SelectCity from './selectCity'
-export default function App() {
-    return <div>
-        <SelectCity />
-        <WeatherView />
-    </div>
-}
